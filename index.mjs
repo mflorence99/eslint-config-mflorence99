406312ce77d514e84a4b07950f11625bb646f82c
Mark Florence <mflo999@gmail.com>
2024-12-07 05:22:15 -0500

    Added tseslint.configs.recommendedTypeChecked

--- index.mjs
+++ index.mjs
@@ -18,7 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: eslint.configs.recommended,
+  recommendedConfig: eslint.configs.recommendedTypeChecked,
   allConfig: eslint.configs.all
 });
 
